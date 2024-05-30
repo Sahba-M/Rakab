85883d023470575485901a85685c5f779cb5dcd5
Sahba-M <sahba.mir.2005@gmail.com>
2024-05-30 15:38:29 +0330

    player.cpp created and functions is written

---
+++ player.cpp
@@ -0,0 +1,33 @@
+#include "player.h"
+
+  Player::Player(){};
+  Player::Player ( int age , std::string name , std::string color ) 
+  {
+    setAge( age );
+    setName( name );
+    setColor( color );
+  }
+  void Player::setName ( std::string name )
+  {
+      this->name = name ;
+  }
+  void Player::setAge ( int age )
+  {
+      this->age = age ;
+  }
+  void Player::setColor ( std::string color )
+  {
+      this->color = color ;
+  }
+  int Player::getAge() const 
+  {
+      return age ;
+  }
+  std::string Player::getName() const 
+  {
+      return name ;
+  }
+  std::string Player::getColor() const 
+  {
+      return color ;
+  }
