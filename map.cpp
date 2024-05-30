506fedb3c7178f9004d3e9d44899a883a35bc75b
Sahba-M <sahba.mir.2005@gmail.com>
2024-05-30 16:33:14 +0330

    readMatrix is written

--- map.cpp
+++ map.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <fstream>
 
 class Map {
     public:
-        void readMatrix ();
+        void readMatrix ()
+        {
+            std::ifstream input ("matrix") ; 
+            if (!input.is_open()) 
+                std::cerr << "Unable to open file !" << std::endl ;
+            int number;
+            while (input) 
+            {
+                std::vector <int> row;
+                for ( int i = 0 ; i < 3 ; i++ ) 
+                {
+                    if ( input >> number )   
+                    {
+                    row.push_back(number);
+                    }
+                }
+                if (!row.empty()) 
+                {
+                adjancencyMatrix.push_back(row);
+                }
+            }
+            input.close();  
+        }
         void readUnorderedMap ();
         bool checkAdjacent ();
 
