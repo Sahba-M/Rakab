61a343b70e566d1b3074e5355b754663272e843d
Sahba-M <sahba.mir.2005@gmail.com>
2024-05-30 16:02:13 +0330

    data members and functionNames written

--- map.cpp
+++ map.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
 class Map {
     public:
-
+        void setMatrix ();
+        void setMap ();
+        bool checkAdjacent ();
 
     private:
+        std::vector < std::vector <int> > adjancencyMatrix ;
+        std::unordered_map < std::string , int > provinceMap ;
 
 };
