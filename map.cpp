ca6aff7b29a697a4c487053ecbcc35c0439c6d2e
Fery <Fery2004@saeedi.com>
2024-06-01 14:32:23 +0330

    test commit

--- map.cpp
+++ map.cpp
@@ -1,50 +1,60 @@
-#include "map.h"
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <fstream>
 
-void Map::readMatrix ()
+class Map
 {
-    std::ifstream input ("matrix") ; 
-    if (!input.is_open()) 
-        std::cerr << "Unable to open file !" << std::endl ;
-    int number;
-    while (input) 
+public:
+    void readMatrix()
     {
-        std::vector <int> row;
-        for ( int i = 0 ; i < 3 ; i++ ) 
+        std::ifstream input("matrix");
+        if (!input.is_open())
+            std::cerr << "Unable to open file !" << std::endl;
+        int number;
+        while (input)
         {
-            if ( input >> number )   
+            std::vector<int> row;
+            for (int i = 0; i < 3; i++)
             {
-            row.push_back(number);
+                if (input >> number)
+                {
+                    row.push_back(number);
+                }
+            }
+            if (!row.empty())
+            {
+                adjacencyMatrix.push_back(row);
             }
         }
-        if (!row.empty()) 
-        {
-        adjacencyMatrix.push_back(row);
-        }
+        input.close();
     }
-    input.close();  
-}
-void Map::readUnorderedMap ()
-{
-    std::ifstream input ; 
-    input.open("map.txt") ;
-    if (!input.is_open()) 
-        std::cerr << "Unable to open file !" << std::endl ;
-        
-    std::string key ;
-    int value ;
-    while (input)
+    void readUnorderedMap()
     {
-        if ( input >> value >> key )
-        {
-            provinceMap[key] = value ;
-        }
-    }
-    input.close();
-}
-bool Map::checkAdjacent ( const std::string & p1 , const std::string & p2 )
-{
-    int index1 = provinceMap[p1];
-    int index2 = provinceMap[p2];
-    return adjacencyMatrix[index1][index2] == 1;
-}
+        std::ifstream input;
+        input.open("map.txt");
+        if (!input.is_open())
+            std::cerr << "Unable to open file !" << std::endl;
+        std::string key;
+        int value;
 
+        while (input)
+        {
+            if (input >> value >> key)
+            {
+                provinceMap[key] = value;
+            }
+        }
+        input.close();
+    }
+    bool checkAdjacent(const std::string &p1, const std::string &p2)
+    {
+        int index1 = provinceMap[p1];
+        int index2 = provinceMap[p2];
+        return adjacencyMatrix[index1][index2] == 1;
+    }
+
+private:
+    std::vector<std::vector<int>> adjacencyMatrix;
+    std::unordered_map<std::string, int> provinceMap;
+};
