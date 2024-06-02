fdb8d1394fa976f4020b6b08906f02bec0cee087
Fery <Fery2004@saeedi.com>
2024-06-02 13:57:57 +0330

    purple.h created

---
+++ purple.h
@@ -0,0 +1,17 @@
+#include "card.h"
+
+#ifndef PURPLE_H
+#define PURPLE_H
+
+
+class PurpleCard : public Card{
+
+    public:
+    // virtual void use();
+
+    private:
+    std::string name;
+
+};
+
+#endif 
