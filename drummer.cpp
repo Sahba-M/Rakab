81b04cda4b167c2e30fee28cda6528346fc9a0dd
Sahba-M <sahba.mir.2005@gmail.com>
2024-06-11 22:48:04 +0330

    functions in class drummer updated

--- drummer.cpp
+++ drummer.cpp
@@ -8,8 +8,20 @@ DrummerCard::DrummerCard(){}
 
 void DrummerCard::useCard ( Player & player )
 {
-  
+    int sum = 0 ;
+    std::vector<int> scores ;
+    std::vector<std::shared_ptr<Card>> Ycards = player.getYcards();
+    for ( auto & card : Ycards )
+    {
+        scores.push_back(stoi(card->getName()));
+    }
+    for ( auto & num : scores )
+    {
+        sum += num ;
+    }
+    player.setScorePlayer(sum);
 }
+
 
 void DrummerCard::setPriority()
 {
