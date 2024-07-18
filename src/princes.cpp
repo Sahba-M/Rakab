b1c4e2c4dbbdecd3adf6a0f5aaee31fcb4e4a2a3
Fery <Fery2004@saeedi.com>
2024-07-18 15:35:29 +0330

    debug

--- src/princes.cpp
+++ src/princes.cpp
@@ -2,11 +2,8 @@
 
 #include "princes.h"
 
-#ifndef PRINCES_H
-#define PRINCES_H
 
 void PrincesCard::useCard ( std::vector <Player> & players, int numPlayer ) 
 {
   players[numPlayer].setScorePlayer(players[numPlayer].getScorePlayer() + 10);
 }
-#endif
